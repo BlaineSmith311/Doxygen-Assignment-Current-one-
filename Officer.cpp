5d3457de943cdc8a881b377f590b8fd9729d54de
BlaineSmith311 <bsmit311@ut.utm.edu>
2026-04-17 23:28:30 -0500

    Add files via upload

--- Officer.cpp
+++ Officer.cpp
@@ -3,19 +3,53 @@
 
 using namespace std;
 
+/**
+ * @file Officer.cpp
+ * @brief Implements the Officer class functions.
+ */
+
+/**
+ * @brief Default constructor for Officer.
+ * 
+ * Initializes evilness to a default value of 500.
+ */
 Officer::Officer() {
   evilness = 500;
 }
 
-Officer::Officer(int ID, int years, double hourlyRate, float hoursWorked, double evilness) : Employee(ID, years, hourlyRate, hoursWorked) {
+/**
+ * @brief Parameterized constructor for Officer.
+ * 
+ * Initializes Officer with employee data and evilness level.
+ * 
+ * @param ID The officer's ID.
+ * @param years Years worked.
+ * @param hourlyRate Hourly pay rate.
+ * @param hoursWorked Hours worked.
+ * @param evilness The officer's evilness level.
+ */
+Officer::Officer(int ID, int years, double hourlyRate, float hoursWorked, double evilness) 
+  : Employee(ID, years, hourlyRate, hoursWorked) {
   this->evilness = evilness;
 }
 
+/**
+ * @brief Prints officer details.
+ * 
+ * Calls base Employee print function and then prints evilness level.
+ */
 void Officer::print() {
   Employee::print();
   cout << " Evilness: " << evilness << endl;
 }
 
+/**
+ * @brief Calculates officer pay.
+ * 
+ * Pay is based on hourly rate plus evilness factor multiplied by hours worked.
+ * 
+ * @return The calculated pay.
+ */
 double Officer::calculatePay() {
   return (hourlyRate + evilness) * hoursWorked;
 }
