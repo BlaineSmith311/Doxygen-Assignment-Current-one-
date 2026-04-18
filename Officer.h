9d176aa49806e29fa8bcb16967d759fce980951a
BlaineSmith311 <bsmit311@ut.utm.edu>
2026-04-17 23:27:55 -0500

    Add files via upload

--- Officer.h
+++ Officer.h
@@ -3,13 +3,47 @@
 
 #include "Employee.h"
 
+/**
+ * @file Officer.h
+ * @brief Defines the Officer class, which extends Employee.
+ */
+
+/**
+ * @class Officer
+ * @brief Represents an officer employee with an added "evilness" factor.
+ * 
+ * Inherits from Employee and modifies pay based on an additional attribute.
+ */
 class Officer : public Employee {
  private:
-  double evilness;
+  double evilness; /**< Represents the officer's evilness level */
+
  public:
+  /**
+   * @brief Prints officer details.
+   */
   void print();
+
+  /**
+   * @brief Calculates the officer's pay.
+   * @return The calculated pay as a double.
+   */
   double calculatePay();
+
+  /**
+   * @brief Default constructor for Officer.
+   */
   Officer();
+
+  /**
+   * @brief Parameterized constructor for Officer.
+   * 
+   * @param ID The officer's ID.
+   * @param years Years worked.
+   * @param hourlyRate Hourly pay rate.
+   * @param hoursWorked Hours worked.
+   * @param evilness The officer's evilness level.
+   */
   Officer(int ID, int years, double hourlyRate, float hoursWorked, double evilness);
 };
 
