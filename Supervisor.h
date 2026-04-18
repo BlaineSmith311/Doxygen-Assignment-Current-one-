a224a8c8bd9db4e34e71d2ad4c63e452cfdc2957
BlaineSmith311 <bsmit311@ut.utm.edu>
2026-04-17 23:27:18 -0500

    Add files via upload

--- Supervisor.h
+++ Supervisor.h
@@ -3,13 +3,48 @@
 
 #include "Employee.h"
 
+/**
+ * @file Supervisor.h
+ * @brief Defines the Supervisor class, which extends Employee.
+ */
+
+/**
+ * @class Supervisor
+ * @brief Represents a supervisor who manages employees.
+ * 
+ * Inherits from the Employee class and adds functionality
+ * for tracking the number of employees supervised.
+ */
 class Supervisor : public Employee {
  private:
-  int numSupervised;
+  int numSupervised; /**< Number of employees supervised */
+
  public:
+  /**
+   * @brief Prints supervisor details.
+   */
   void print();
+
+  /**
+   * @brief Calculates the supervisor's pay.
+   * @return The calculated pay as a double.
+   */
   double calculatePay();
+
+  /**
+   * @brief Default constructor for Supervisor.
+   */
   Supervisor();
+
+  /**
+   * @brief Parameterized constructor for Supervisor.
+   * 
+   * @param ID The supervisor's ID.
+   * @param years Years worked.
+   * @param hourlyRate Hourly pay rate.
+   * @param hoursWorked Number of hours worked.
+   * @param numSupervised Number of employees supervised.
+   */
   Supervisor(int ID, int years, double hourlyRate, float hoursWorked, int numSupervised);
 };
 
