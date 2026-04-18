ec7705d5c7e0664d84d37bd7568103462a8c94c4
BlaineSmith311 <bsmit311@ut.utm.edu>
2026-04-17 23:30:03 -0500

    Add files via upload

--- Employee.h
+++ Employee.h
@@ -1,19 +1,55 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
 
+/**
+ * @file Employee.h
+ * @brief Defines the base Employee class.
+ */
+
+/**
+ * @class Employee
+ * @brief Represents a basic employee with ID, years worked, hourly rate, and hours worked.
+ * 
+ * This is a base class intended to be extended by other roles such as Supervisor and Officer.
+ */
 class Employee {
  private:
-  int ID; //employee ID
-  int years;
+  int ID;      /**< Employee ID */
+  int years;   /**< Number of years employed */
   
  protected:
-  double hourlyRate;
-  float hoursWorked;
+  double hourlyRate;   /**< Employee hourly pay rate */
+  float hoursWorked;   /**< Number of hours worked */
+
  public:
+  /**
+   * @brief Prints employee information.
+   */
   virtual void print();
+
+  /**
+   * @brief Calculates employee pay.
+   * @return Total pay based on hourly rate and hours worked.
+   */
   virtual double calculatePay();
+
+  /**
+   * @brief Updates employee anniversary information.
+   */
   void anniversary();
+
+  /**
+   * @brief Default constructor for Employee.
+   */
   Employee();
+
+  /**
+   * @brief Parameterized constructor for Employee.
+   * @param ID Employee ID
+   * @param years Years employed
+   * @param hourlyRate Hourly pay rate
+   * @param hoursWorked Hours worked
+   */
   Employee(int ID, int years, double hourlyRate, float hoursWorked);
 };
 
